3bf9e19862c014743050f3bca3c3a246a2291067
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-11-11 23:25:24 +0200

    [cms] env

--- projects/cms/src/env/index.ts
+++ projects/cms/src/env/index.ts
@@ -1,9 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false
+// this file will be replaced with other files (ex: prod.ts) with `ng build --prod`
+//angular.json -> fileReplacements
+export default {
+  mode: "dev"
 };
 
 /*
--- projects/cms/src/main.ts
+++ projects/cms/src/main.ts
@@ -2,11 +2,9 @@ import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app/app.module";
-import { environment } from "./environments/environment";
+import env from "./env";
 
-if (environment.production) {
-  enableProdMode();
-}
+if (env.mode === "prod") enableProdMode();
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
