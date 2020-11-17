cae49b53bfcd9d958d1787d5d1bc80d3ff70c02b
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-11-17 19:03:41 +0200

    [cms] fix paths & vars

--- projects/cms/src/server/main.ts
+++ projects/cms/src/server/main.ts
@@ -1,10 +1,9 @@
 import { enableProdMode } from "@angular/core";
+import env from "../env";
 
-import { environment } from "./environments/environment";
-
-if (environment.production) {
+if (env.mode === "prod") {
   enableProdMode();
 }
 
-export { AppServerModule } from "./app/app.server.module";
+export { AppServerModule } from "../app/app.server.module";
 export { renderModule, renderModuleFactory } from "@angular/platform-server";
