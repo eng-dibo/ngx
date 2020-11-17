6b21e1d80eb6128f357d1122485b4e148d66b19e
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-11-17 21:10:57 +0200

    [cms] root path alias for webpack, tsconfig
    - set the root path alias for webpack, tsconfig
    
     (ex: ~path)
    
    - use '@angular-builders/custom-webpack' to customise webpack options
    
    - move tsconfig.server.ts to server/tsconfig.ts
    
    - webpack options:
      - execlude node_modules from bundling

---
+++ projects/cms/src/server/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = (config, options) => {
+  //change 'mode' by env (ex: npx cross-env NODE_ENV=production ...)
+  config.mode = "none";
+
+  //change the entry defined in angular.json/**/server/main
+  //the output will be dist/**/express.js instead of main.js
+  config.entry = { express: "./src/server/express.ts" };
+
+  //execlude node_modules from bundling, use require(package) instead.
+  config.externals.push(nodeExternals());
+
+  //set the root path alias(ex: ~packages/*)
+  //tsconfig.json/**/path sets the alias for typescript,
+  //use config.resolve.alias for webpack
+  config.resolve.alias["~"] = path.resolve(__dirname, "../../../../");
+
+  //fix: setting library & libraryTarget to fix issue: require('./server.js') == undefined
+  //https://github.com/webpack/webpack/issues/2030#issuecomment-232886608
+  //https://github.com/webpack/webpack/issues/2030#issuecomment-290363910
+  //todo: libraryTarget commonjs VS commonjs-module
+  config.output.library = "";
+  config.output.libraryTarget = "commonjs-module";
+  config.module.rules.push(
+    { test: /\.ts$/, loader: "ts-loader" },
+    {
+      //load .node files
+      //ex: ./node_modules/sharp/build/Release/sharp.node
+      // https://github.com/lovell/sharp/issues/794#issuecomment-307188099
+      test: /\.node$/,
+      use: "node-loader"
+    }
+  );
+
+  //optimization: {minimize: false},
+  //module.noParse: /polyfills-.*\.js/,
+  return config;
+};
