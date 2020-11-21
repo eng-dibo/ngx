7133c09fdd0b5d9176bbd5c50a22028c9e43386a
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-11-21 21:33:55 +0200

    [cms] merge server/app.server.module & server/main

--- projects/cms/server/main.ts
+++ projects/cms/server/main.ts
@@ -1,9 +1,17 @@
 import { enableProdMode } from "@angular/core";
 import env from "../src/env";
+import { NgModule } from "@angular/core";
+import { ServerModule } from "@angular/platform-server";
+import { AppModule } from "../src/app/app.module";
+import { AppComponent } from "../src/app/app.component";
 
 if (env.mode === "prod") {
   enableProdMode();
 }
 
-export { AppServerModule } from "./app.server.module";
+@NgModule({
+  imports: [AppModule, ServerModule],
+  bootstrap: [AppComponent]
+})
+export class AppServerModule {}
 export { renderModule, renderModuleFactory } from "@angular/platform-server";
