7e769798c0096f7c878592a3947e339d25e00a60
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-11-21 22:21:59 +0200

    [cms] error component

--- projects/cms/src/app/app.module.ts
+++ projects/cms/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ErrorComponent } from './error.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ErrorComponent],
   imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
