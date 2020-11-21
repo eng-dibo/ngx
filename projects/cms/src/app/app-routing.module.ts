3a3a36917e8228159ffdf27c97d6f5bf5acba56b
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-11-21 21:59:55 +0200

    [cms] app-routing.module enableTracing in dev mode

--- projects/cms/src/app/app-routing.module.ts
+++ projects/cms/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import env from "../env";
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
-})],
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: "enabled",
+      enableTracing: env.mode === "dev"
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
