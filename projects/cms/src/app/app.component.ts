2ea1bb36966dcb7f2695284e92b65f39b757dcc1
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-11-21 22:02:48 +0200

    [cms] app.component go(path)

--- projects/cms/src/app/app.component.ts
+++ projects/cms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,10 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "cms";
+
+  constructor(private router: Router) {}
+  go(path: Array | string) {
+    if (typeof path == "string") path = [path];
+    this.router.navigate(path);
+  }
 }
