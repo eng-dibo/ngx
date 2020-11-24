5b6c782e2254ef8571cc800b24f002a97ab097c2
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-11-25 00:25:30 +0200

    [cms] rename src to browser

--- projects/cms/server/main.ts
+++ projects/cms/server/main.ts
@@ -2,8 +2,8 @@ import { enableProdMode } from "@angular/core";
 import env from "~config/env";
 import { NgModule } from "@angular/core";
 import { ServerModule } from "@angular/platform-server";
-import { AppModule } from "~src/app/app.module";
-import { AppComponent } from "~src/app/app.component";
+import { AppModule } from "~browser/app/app.module";
+import { AppComponent } from "~browser/app/app.component";
 
 if (env.mode === "prod") {
   enableProdMode();
