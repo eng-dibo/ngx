ec08a2387ba2f2007a46caaff92b78c56fa48f88
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-12-10 19:23:21 +0200

    [cms] {minor} fix import path

--- projects/cms/browser/app/app.component.ts
+++ projects/cms/browser/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
-import { metaTags } from "~config/front";
+import { metaTags } from "~config/browser";
 
 @Component({
   selector: "app-root",
