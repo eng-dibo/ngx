03300809cc7f5636bfdfe10276f693a584fadff9
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-12-10 15:40:56 +0200

    [cms] server/ mongooe, functions

---
+++ projects/cms/server/functions.ts
@@ -0,0 +1,155 @@
+import { connect, getModel } from "./mongoose/functions";
+
+import {
+  cache,
+  mdir,
+  ext,
+  parsePath,
+  rename,
+  unlink,
+  readdir,
+  resolve,
+  writeFile,
+  json
+} from "@engineers/nodejs-tools/fs";
+import { statSync, renameSync, writeFileSync, existsSync } from "fs";
+
+import { Firebase } from "@engineers/firebase/admin";
+import { initializeApp } from "firebase-admin";
+import multer from "multer";
+
+import { Categories } from "@engineers/ngx-formly/categories-material/functions";
+import { setTimer, getTimer, endTimer } from "@engineers/nodejs-tools/timer";
+import { TEMP } from "~config/server";
+import { FIREBASE } from "~config/firebase";
+
+export const dev = process.env.NODE_ENV === "development";
+
+/*
+process.cwd() for firebase = functions.source (i.e: ./dist)
+whitch is different from here (i.e /)
+so using process.cwd() will give different values for
+`npm run start` and `npm run firebase:serve`
+
+process.env.INIT_CWD is the path where `npm run` runned, regardless of the current working directory
+you must run `npm start` or `npm firebase:serve` from the project's root dir.
+
+another solution is to replace process.cwd() with it's value when building this file with webpack
+to be a fixed value in both cases.
+
+or re-set the view dir in firebase/index to be relative to it's process.cwd() dir
+i.e functions.source (./dist)
+app.set("views", "./browser");
+
+todo: process.env.INIT_CWD || ?? -> check if process.env.INIT_CWD is undefined
+*/
+
+//todo: use env:GOOGLE_APPLICATION_CREDENTIALS=Path.resolve("./firebase-almogtama3-eg.json")
+
+export const bucket = new Firebase(FIREBASE).storage();
+/*
+
+
+/**
+ * get adjusted categories (i.e: adding branches, top to each entry & add main categories)
+ * & adjusted articles_categories (i.e: article_categories & category_articles)
+ * & inputs (for forms)
+ * @method categories
+ * @return {categories, main, article_categories, category_articles, inputs}
+ */
+export function getCategories(collection: string = "articles") {
+  return cache(`./temp/${collection}/categories.json`, () =>
+    connect().then(() => {
+      setTimer("getCategories");
+      return Promise.all([
+        getModel(`${collection}_categories`)
+          .find({})
+          .lean(),
+        //get all topics categories
+        getModel(collection)
+          .find({}, "categories")
+          .lean()
+      ])
+        .then(([categories, items]) => {
+          if (dev)
+            console.log(
+              "[server] getCategories: fetched from server",
+              getTimer("getCategories")
+            );
+          /*
+          //don't close the connection after every query
+          //todo: close the connection when the server restarts or shutdown
+          //https://hashnode.com/post/do-we-need-to-close-mongoose-connection-cjetx0dxh003hcws2l1fs81nl
+          mongoose.connection.close(() => {
+            if (dev) console.log("connection closed");
+          });
+          */
+          let ctg = new Categories(categories);
+          ctg.adjust();
+          if (dev)
+            console.log(
+              "[server] getCategories: adjusted",
+              endTimer("getCategories")
+            );
+          return ctg.itemCategories(items);
+        })
+        .catch(err => {
+          console.error("Error @categories", err);
+          throw new Error(`Error @categories, ${err.message}`);
+        });
+    }, 1)
+  );
+}
+
+export const jsonData = {
+  get(type: string, id?: string | Number) {
+    let file = `${TEMP}/${type}/${id ? id + "/data" : "index"}.json`;
+    try {
+      return json.read(file);
+    } catch (e) {
+      console.warn(`jsonData.get(${type},${id}) failed`, e);
+    }
+  },
+  save(type: string, data) {
+    if (data) {
+      let dir = `${TEMP}/${type}`;
+      mdir(dir);
+      if (data instanceof Array) json.write(`${dir}/index.json`, data);
+      else json.write(`${dir}/${data._id}/data.json`, data);
+    }
+  }
+};
+
+//multer hanles multipart/form-data ONLY, make sure to add enctype="multipart/form-data" to <form>
+//todo: add multer to specific urls: app.post(url,multer,(req,res)=>{})
+//todo: if(error)res.json(error)
+//todo: fn upload(options){return merge(options,defaultOptions)}
+export let upload = multer({
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+    fieldSize: 10 * 1024 * 1024, //form total size; formData[content] contains some images (base64 encoded)
+    files: 20
+  },
+  fileFilter: function(req, file, cb) {
+    //we only upload 'cover image', so only images are available
+    //other files are pasted into quill editor as base64-encoded data.
+    let result = file.mimetype.startsWith("image/");
+    if (dev) console.log("multer fileFilter", { result, req, file, cb });
+    cb(null, result); //to reject this file cb(null,false) or cb(new error(..))
+  },
+  //multer uses memoryStorage by default
+  //diskStorage saves the uploaded file to the default temp dir,
+  //but rename(c:/oldPath, d:/newPath) not allowed,
+  //so we upload the file to a temporary dir inside the same partition
+  //https://stackoverflow.com/a/43206506/12577650
+  storage: multer.memoryStorage() /*multer.diskStorage({
+    destination: function(req, file, cb) {
+      let dir = `${TEMP}/uploads`;
+      mdir(dir);
+      cb(null, dir);
+    },
+    filename: function(req, file, cb) {
+      cb(null, `tmp${new Date().getTime()}.tmp`);
+    }
+  }) */
+});
---
+++ projects/cms/server/mongoose.ts
@@ -0,0 +1,58 @@
+import {
+  connect as _connect,
+  model,
+  mongoose,
+  types
+} from "@engineers/mongoose";
+import { schemas, DB } from "~config/server";
+
+/**
+ * connect to the database using 'config'
+ * @method connect
+ * @param  uri
+ * @return
+ */
+export function connect(uri?: types.uri) {
+  if (!uri)
+    uri = {
+      auth: DB.auth,
+      host: DB.host,
+      srv: DB.srv,
+      dbName: DB.dbName
+    };
+
+  return _connect(uri, { multiple: false });
+}
+
+/**
+ * close all connections;  OR: mongoose.connection.close()
+ * @method disconnect
+ * @return [description]
+ */
+export function disconnect() {
+  return mongoose.disconnect();
+}
+
+/**
+ * convert an object to a mongoose model
+ * @method getModel
+ * @param  collection   [description]
+ * @param  schemaObj={} [description]
+ * @return [description]
+ */
+export function getModel(collection, schemaObj = {}) {
+  //console.log("model: " +{ type, models: mongoose.models, modelNames: mongoose.modelNames() });
+  //todo: schemas/mongoose.ts
+
+  if (!schemaObj) {
+    //schemaName is the same as collection name (except if [collection]_categories)
+    //ex: articles_categories, jobs_categories
+    let schemaName = collection.indexOf("_categories")
+      ? "categories"
+      : collection;
+    schemaObj =
+      schemaName in schemas ? schemas[schemaName] : schemas["basic"] || {};
+  }
+
+  return model(collection, schemaObj, { strict: false });
+}
