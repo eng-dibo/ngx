ef82b087bf64d886d0865e00a331090124d90d25
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-01-09 19:19:51 +0200

    [cms: server] webpack

--- projects/cms/server/webpack.config.js
+++ projects/cms/server/webpack.config.js
@@ -1,12 +1,14 @@
 const basicConfig = require("../webpack.config.js");
+const path = require("path");
 
 module.exports = (config, options) => {
   options.target = "server";
   config = basicConfig(config, options);
 
   //changes the entry defined in angular.json/**/server/main
-  //paths are related to angular.json/projects/cms
+  //paths are related to angular.json (i.e: ./projects/cms/server/*.ts)
   //the output will be dist/**/express.js instead of main.js
-  config.entry = { express: "./server/express.ts" };
+  config.entry = { express: path.join(__dirname, "./express.ts") };
+  //console.log({ config, options });
   return config;
 };
