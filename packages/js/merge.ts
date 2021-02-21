0b75d5601f14d161c1d283a3b6fadd4b6b5c87e2
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-02-22 00:52:26 +0200

    [pkg: js] merge
    - add Strategy:ignore
    
    
    - temporary remove Strategy: push, unshift from strings
    
    - fix
    
    - merge/deepMerge()

--- packages/js/merge.ts
+++ packages/js/merge.ts
@@ -1,7 +1,7 @@
 //todo: use @engineers/*
 import { objectType, cleanObject } from "../nodejs/objects";
 
-export type Strategy = "replace" | "push" | "unshift";
+export type Strategy = "replace" | "ignore" | "push" | "unshift";
 export interface MergeOptions {
   //push & unshift have the same effect for objects
   strategy?: Strategy;
@@ -37,6 +37,7 @@ export function merge(elements: Array<any>, options: MergeOptions = {}) {}
 //todo: https://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge/34749873
 //todo: return type of elements[0]; deepMerge<T>(elements:Array<T,...any>):T{}
 //https://stackoverflow.com/questions/44402096/in-typescript-define-a-type-for-an-array-where-the-first-element-is-more-specif
+//todo: rules={[key:string]: Strategy|Rules}
 export function deepMerge(
   elements: Array<any>,
   options: MergeOptions = {}
@@ -46,7 +47,7 @@ export function deepMerge(
     level: 0
   };
   //make 'options' imutable
-  let opts = Object.assign({}, options, defaultOptions),
+  let opts = Object.assign({}, defaultOptions, options),
     result = elements.shift(),
     type = objectType(result);
 
@@ -56,29 +57,35 @@ export function deepMerge(
         //ex: deepMerge([{k:{a:1}}, {k:{b:1}}]) -> {k:{a:1,b:1}}
         el = cleanObject(el);
         for (let k in el) {
-          if (
-            !(
-              objectType(result[k]) === "object" &&
-              objectType(el[k]) === "object"
-            ) ||
-            opts.strategy === "replace" ||
-            !(k in result)
-          )
-            result[k] = el[k];
+          if (opts.strategy === "replace" || !(k in result)) result[k] = el[k];
           else result[k] = deepMerge([result[k], el[k]], opts);
         }
       } else if (type === "array") {
-        //ex: deepMerge([1,2], [3,4]) -> [1,2,3,4] | [3,4,1,2]
-        result[<Strategy>opts.strategy](...el);
+        //ex: deepMerge([1,2], [3,4]) -> push:[1,2,3,4] | unshift:[3,4,1,2] | replace: [3,4] | ignore: [1,2]
+        if (opts.strategy === "ignore") return;
+        else if (opts.strategy === "replace") result = el;
+        //result.push() or result.unshift()
+        else result[<Strategy>opts.strategy](...el);
       } else if (type === "string") {
         //ex: deepMerge(["ab", "cd"]) -> "abcd" | "cdab"
-        result = opts.strategy === "unshift" ? el + result : result + el;
+        if (opts.strategy === "ignore") return;
+        if (opts.strategy === "replace") result = el;
+        //todo: {tmp}, temporary replace strings untill rules{} be implemented
+        else result = el;
+        //else result = opts.strategy === "push" ? result + el : el + result;
       }
     } else {
       //ex: merge([{x:1}, y]) -> {x:1, y:true}
       //ex: merge([ [1,2], 3 ]) -> [1,2,3]  (i.e: Array.push())
       //if(opts.strict): error
-      //todo:
+      //todo: implement this part
+
+      if (type === "object") {
+      } else if (type === "array") {
+      }
+
+      //temp:
+      result = el;
     }
   });
 
