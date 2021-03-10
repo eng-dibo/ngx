40341c1110ac8f6cd21d1134c239e7f055d8c27a
eng-dibo <sh.eldeeb.2010@gmail.com>
2021-03-10 22:34:42 +0000

    [pkg: cli-helper] allow named params

--- packages/cli-helper/index.ts
+++ packages/cli-helper/index.ts
@@ -1,6 +1,15 @@
-const fs = require('fs');
-import { execSync } from 'child_process';
 import { argv } from 'process';
+import parseArgv, {Opts, ParsedArgs} from 'minimist';
+
+
+// Partial<> converts all required properties into optional,
+// so we can delete the property '_
+// todo: only convert '_' to be optional
+export interface ParsedArgsOptional extends Partial<ParsedArgs> {}
+
+export function parse(options?: Opts): ParsedArgsOptional{
+  return parseArgv(argv.slice(2), options);
+}
 
 export interface Tasks{
     // todo: ()=>any
@@ -11,24 +20,25 @@ export interface Tasks{
  *
  * @param tasks
  *
- * todo: eable named params, ex: > tasks taskName paramValue --paramName value2
  */
 export function runTask(tasks: Tasks ): void{
-let task = argv.slice(2)[0], params = argv.slice(3);
+// let task = argv.slice(2)[0], params = argv.slice(3);
+
+let parsedArgs = parse(), task = parsedArgs._![0] , params = parsedArgs._!.slice(1);
+delete parsedArgs._;
+// console.log({task, params, options: parsedArgs});
+
 if (!task) {throw new Error('task not provided!'); }
 else if (!(task in tasks)) {throw new Error(`unknown task ${task}`); }
 
 try {
     console.log(`>> running the task: ${task}`);
-    // params.forEach(el => console.log(`  > ${el}`));
-    tasks[task](...params);
+    tasks[task](...params, parsedArgs);
     console.log('>> Done');
   } catch (error) {
     console.error({error});
     throw new Error(`>> error in task ${task}`);
   }
 
-
-console.log({task, params});
 }
 
